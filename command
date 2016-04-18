c043d060360db5c9da77da90d147355b81c2b0f7
zhouwei <weizhou@mail.ustc.edu.cn>
2016-04-18 00:35:36 -0400

    add

---
+++ command
@@ -0,0 +1,5 @@
+ssh -f -n -N -D 0.0.0.0:1080 localhost >& $(pwd)/socks_proxy.log </dev/null 
+
+ng 1080 | awk '{print $5}' | awk -F: '{print $1}' | sort -r | uniq -c 
+
+curl http://ip.cn/index.php?ip=124.128.56.224 | grep 来自 | awk -F： '{print $3}'
---
+++ stats.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+netstat -anp | grep 1080 | awk '{print $5}' | awk -F: '{print $1}' | sort -r | uniq -c | grep -v "0.0.0.0" > tmp1
+
+while read line
+do
+    echo -n "$line  "
+    ip=$(echo -n $line | awk '{print $2}')
+    curl -s http://ip.cn/index.php?ip=${ip} | grep 来自 | awk -F： '{print $3}'
+done < tmp1
+
+rm -f tmp1 >&/dev/null
